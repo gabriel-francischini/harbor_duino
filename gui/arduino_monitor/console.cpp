3e772851f8a76ba89e9988561bce4bf9c7c85f38
Gabriel Francischini <empresagabriel@gmail.com>
2016-03-15 10:47:03 -0300

    Agora o console está mais parecido com um terminal.

--- gui/arduino_monitor/console.cpp
+++ gui/arduino_monitor/console.cpp
@@ -1,5 +1,21 @@
 #include "console.h"
 
+// Construtor do console
 Console::Console(){
-    this->setCursorWidth(7);
+
+	// Muda a largura do cursor,
+	// para ficar mais "quadrado"
+	int cursor_value = 10;
+	this->setCursorWidth(cursor_value);
+	this->setCurrentFont(QFont("Consolas", cursor_value));
+
+	QPalette colors = this->palette();
+	colors.setColor(QPalette::Base, Qt::black);
+	colors.setColor(QPalette::Text, Qt::green);
+	colors.setColor(QPalette::AlternateBase, Qt::white);
+	colors.setColor(QPalette::Highlight, Qt::blue);
+	colors.setColor(QPalette::HighlightedText, Qt::white);
+	this->setPalette(colors);
+
+	this->lineWrapMode();
 }
